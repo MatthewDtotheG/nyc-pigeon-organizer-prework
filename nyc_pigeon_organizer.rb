ac947e482b6677690af8a44ef645b4a57d1284b0
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-28 18:16:55 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,11 +11,11 @@ def nyc_pigeon_organizer(data)
           pigeon_data[language] = {}  #initiate a new hash for the value of :ruby
         end
         # this would cause us problems if we had more than one attribute we're iterating over
-        language_attributes[language][:style] ||= []  # create a :style key within that hash and set it equal to an empty array
-        language_attributes[language][:style] << oo_or_functional  #added oo key from first level of languages hash to this key
-        if language_attributes[language][attribute].nil?
+        pigeon_data[language][:style] ||= []  # create a :style key within that hash and set it equal to an empty array
+        pigeon_data[language][:style] << oo_or_functional  #added oo key from first level of languages hash to this key
+        if pigeon_data[language][attribute].nil?
           #if :ruby language key doesn't have a :type key nested within it
-          language_attributes[language][attribute] = str_value  #set this :type key to equal the str "interpreted"
+          pigeon_data[language][attribute] = str_value  #set this :type key to equal the str "interpreted"
         end
       end
     end
